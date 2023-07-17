23c582df83e5e4ebce15f2708e4ecfc248a8a2a3
Anucha Chewachanon <68524576+SpiralNuggets@users.noreply.github.com>
2023-07-17 16:39:01 +0700

    Added the fruits forms

---
+++ src/routes/kyubeyforms/fruit/+page.svelte
@@ -0,0 +1,32 @@
+<html lang=en>
+
+<head>
+    <title>Sis Puella Magica! slash fruit</title>
+</head>
+
+<body>
+
+    <h1 class="text-primary text-4xl font-bold text-center">Sis Puella Magica! slash fruit</h1>
+    <h3 class="text-primary-content text-2xl text-center">GitHub don't take this down! it's my assignment!</h3>
+
+<form class = "text-center">
+    <label for="fruits">Select your favorite fruits:</label><br>
+  
+    <select id="fruits" name="fruits" class="select select-primary" multiple>
+      <option value="apple" selected>Apple</option>
+      <option value="banana">Banana</option>
+      <option value="cherry">Cherry</option> 
+      <option value="date" selected>Date</option>
+      <option value="elderberry">Elderberry</option>
+    </select>
+    <br>
+  
+    <input class="btn btn-primary btn-outline" type="submit" value="Submit">
+</form>
+
+
+<footer class="footer justify-center">
+    <p>This page was really made by Anucha Cheewachanon.</p>
+</footer>
+</body>
+</html>
